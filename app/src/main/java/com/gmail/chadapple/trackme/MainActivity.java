391437703422dc71037bb861e624ebb1706bcfa1
Chad Apple <chadapple@Chads-MBP.frontier.com>
2015-04-26 08:26:29 -0400

    Polyline working to track history of positions.

--- app/src/main/java/com/gmail/chadapple/trackme/MainActivity.java
+++ app/src/main/java/com/gmail/chadapple/trackme/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.IBinder;
@@ -17,12 +18,18 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.List;
 
 public class MainActivity extends FragmentActivity implements LocationService.LocationCallback {
   private GoogleMap mMap;
   private Marker mLocationMarker = null;
   static private final String TAG = "TrackMeMain";
   private boolean mBound = false;
+  private PolylineOptions mLineOptions = new PolylineOptions();
+  private Polyline mPolyline = null;
 
   private ServiceConnection mServiceConnection = new ServiceConnection() {
     @Override
@@ -85,21 +92,36 @@ public class MainActivity extends FragmentActivity implements LocationService.Lo
     mMap.getUiSettings().setZoomControlsEnabled(true);
     mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(40.058039, -84.136014), 10.0f));
     mMap.addMarker(new MarkerOptions().position(new LatLng(40.058039, -84.136014)).title("My house"));
+    // Polyline will be used to draw a history of locations
+    mLineOptions.color(Color.BLUE);
+    mPolyline = mMap.addPolyline(mLineOptions);
   }
 
   @Override
   public void LocationChanged(Location location) {
-    if (mLocationMarker != null) {
-      mLocationMarker.remove();
-    }
-    else {
+    LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+    if(mLocationMarker == null) {
       // If this is the first update, zoom camera to location
-      mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 13.0f));
+      mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(ll, 13.0f));
     }
-    mLocationMarker = mMap.addMarker(new MarkerOptions().
-        position(new LatLng(location.getLatitude(), location.getLongitude())).
-        title("My Location").
-        icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).
-        draggable(false));
+    List<LatLng> points = mLineOptions.getPoints();
+    float[] distance = new float[] {10000.0f};
+    if(!points.isEmpty()) {
+      LatLng lastPoint = points.get(points.size()-1);
+      Location.distanceBetween(lastPoint.latitude, lastPoint.longitude, ll.latitude, ll.longitude, distance);
+    }
+    // If moved a significant amount of distance, add a polyline and move the marker
+    if(distance[0] > 100.0f) {
+      if (mLocationMarker != null) {
+        mLocationMarker.remove();
+      }
+      mLineOptions.add(ll);
+      mPolyline.setPoints(mLineOptions.getPoints());
+      mLocationMarker = mMap.addMarker(new MarkerOptions().
+              position(ll).
+              title("Current Location").
+              icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).
+              draggable(false));
+    }
   }
 }
