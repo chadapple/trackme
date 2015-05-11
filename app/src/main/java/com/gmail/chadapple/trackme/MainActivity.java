f3daf6190446c8e9f85a7f47fc7f113e97d4dd5c
Chad Apple <chadapple@Chads-MBP.frontier.com>
2015-05-11 19:51:12 -0400

    Added dropdown list with routes pulled from server.

--- app/src/main/java/com/gmail/chadapple/trackme/MainActivity.java
+++ app/src/main/java/com/gmail/chadapple/trackme/MainActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.graphics.Color;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,6 +24,26 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends FragmentActivity implements LocationService.LocationCallback {
@@ -44,11 +67,57 @@ public class MainActivity extends FragmentActivity implements LocationService.Lo
     }
   };
 
+  private class GetRoutes extends AsyncTask<String, String, String> {
+    InputStream inputStream = null;
+    String result = null;
+    protected String doInBackground(String... str) {
+//      String url_select = "http://capple.no-ip.org/trackme/route.php";
+      String url_select = "http://192.168.1.50/trackme/route.php";
+
+      try {
+        URL url = new URL(url_select);
+        URLConnection urlconn = url.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                urlconn.getInputStream()));
+        StringBuilder sBuilder = new StringBuilder();
+        String line = null;
+        while ((line = in.readLine()) != null) {
+          sBuilder.append(line + "\n");
+        }
+        result = sBuilder.toString();
+        in.close();
+      } catch (IOException e1) {
+        Log.e("GetRoutes", e1.toString());
+        e1.printStackTrace();
+      }
+      return result;
+    }
+    protected void onProgressUpdate(String... progress) {
+    }
+    protected void onPostExecute(String result) {
+      //parse JSON data
+      try {
+        JSONArray points = new JSONArray(result);
+        ArrayList<String> stringArrayList = new ArrayList<String>();
+
+        // looping through All Contacts
+        for (int i = 0; i < points.length(); i++) {
+          JSONObject c = points.getJSONObject(i);
+          stringArrayList.add(c.getString("name"));
+        }
+        setupControls(stringArrayList.toArray(new String[stringArrayList.size()]));
+      } catch (JSONException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
     setUpMap();
+    new GetRoutes().execute(new String("test"));
     startService(new Intent(this, LocationService.class));
   }
 
@@ -76,9 +145,16 @@ public class MainActivity extends FragmentActivity implements LocationService.Lo
     setUpMap();
   }
 
-  ////////////////////////////////////////////////////////////////////////////////////////
-  // setUpMap()
-  ////////////////////////////////////////////////////////////////////////////////////////
+  private void setupControls(String[] choices) {
+    // Selection of the spinner
+    Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+    // Application of the Array to the Spinner
+    ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, choices);
+    spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+    spinner.setAdapter(spinnerArrayAdapter);
+  }
+
   private void setUpMap() {
     if (mMap != null) {
       return;
